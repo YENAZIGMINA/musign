52a06e9b90575bf72d63653289d6fc0a98ef0dec
lee_yena <leeyena4724@naver.com>
2023-06-16 17:51:30 +0900

    뮤자인

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//메인메뉴의 움직이는 바
+
+//자바스크립트에서는 .addevnetlistner / 아래는 제이쿼리
+$('.gnb li a').mouseenter(function(){
+    let bar=$(this).position().left;
+    console.log(bar)
+    let widNum=$(this).width();
+    //$('.bar').animate({실행문},시간)
+    $('.bar').animate({
+        'left':bar + "px",
+        'width':widNum + "px",
+        'opacity':1
+    },300)
+})
+
+$('.gnb').mouseleave(function(){
+    $('.bar').animate({
+        'left':0,
+        'width':0,
+        'opacity':0
+    },10)
+})
+
+
+//전체화면 애니메이션 (스크롤라.js사이트 - Custom settings 복붙)
+$('.animate').scrolla({
+    // default
+    mobile: false, // 모바일 버전에서 활성화
+    once: false, // 스크롤시 한 번 또는 여러번 실행 설정
+    animateCssVersion: 4 // animate.css 버전 (3 or 4)
+});
